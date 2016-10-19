a3ffcefcb64af383711478a1dabc64580a5fa9f4
ilia <ilya4041@yandex.ru>
2016-10-19 19:48:52 +0300

    add bracket

--- index.php
+++ index.php
@@ -286,18 +286,6 @@ if ($client->getAccessToken()) {
                         foreach ($data->getRows() as $row)
 echo "[ new Date('{$row->keys[0]}', $row->clicks, $row->impressions, $row->ctr, $row->position],";
                         ?>
-/*                    [new Date(2014, 0, 1),  -.5, -.5, -.5,  5.7],
-                    [new Date(2014, 1),   .4,  .4,  .4,  8.7],
-                    [new Date(2014, 2),   .5,  .5,  .5,   12],
-                    [new Date(2014, 3),  2.9, 2.9, 2.9, 15.3],
-                    [new Date(2014, 4),  6.3, 6.3, 6.3, 18.6],
-                    [new Date(2014, 5),    9,   9,   9, 20.9],
-                    [new Date(2014, 6), 10.6,10.6,10.6, 19.8],
-                    [new Date(2014, 7), 10.3,10.3,10.3, 16.6],
-                    [new Date(2014, 8),  7.4, 7.4, 7.4, 13.3],
-                    [new Date(2014, 9),  4.4, 4.4, 4.4,  9.9],
-                    [new Date(2014, 10), 1.1, 1.1, 1.1,  6.6],
-                    [new Date(2014, 11), -.2, -.2, -.2,  4.5]*/
                 ]);
 
                 var materialOptions = {
