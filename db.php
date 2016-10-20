d7c556f9ccb62c2f4971a5a699fa8ff59caa5416
ilia <ilya4041@yandex.ru>
2016-10-20 19:50:40 +0300

    add db

--- db.php
+++ db.php
@@ -5,10 +5,12 @@ class Db
     const DB_PASS = 'root';
     const DB_NAME = 'googleConsole';
     private $connection;
+    
     public function __construct()
     {
         $this->connect();
     }
+    
     private function connect()
     {
         $this->connection = mysqli_connect('localhost', $this::DB_USER, $this::DB_PASS, $this::DB_NAME);
@@ -16,57 +18,36 @@ class Db
             echo "Failed to connect to MySQL";
         }
     }
-    public function new_action($key, $ip)
+
+    public function is_client_website_exist($client_id, $website){
+        $sql = "SELECT `id` FROM clients WHERE `client_id` = '$client_id' AND site_url = '$website'";
+        $query = mysqli_query($this->connection, $sql);
+        $rows = mysqli_num_rows($query);
+        if( $rows > 0)
+            return $query;
+        return false;
+    }
+
+    public function add_website($client_id, $website)
     {
-        $safe_key = mysqli_real_escape_string($this->connection, $key);
-        $sql = "INSERT INTO actions (`key`,`ip`) VALUES ('$safe_key','$ip')";
+        $sql = "INSERT INTO clients (`client_id`,`site_url`) VALUES ('$client_id','$website')";
         if (!mysqli_query($this->connection, $sql))
             die('Error');
         return $this->connection->insert_id;
     }
-    public function is_wasted_key($key)
-    {
-        $safe_key = mysqli_real_escape_string($this->connection, $key);
-        $sql = "SELECT * FROM actions WHERE `key` = '$safe_key'";
-        $query = mysqli_query($this->connection, $sql);
-        $rows = mysqli_num_rows($query);
-        if( $rows > 0)
-            return true;
-        return false;
+
+    public function get_last_record_date($site_id){
+        $sql = "SELECT `date` FROM `data` WHERE `site_id` = $site_id";
+        return mysqli_query($this->connection, $sql);
     }
-    public function new_listening($key_id, $audio){
-        $sql = "INSERT INTO listening (`actions_id`,`audio`) VALUES ('$key_id','$audio')";
+//"clicks"]=> float(1) ["impressions"]=> float(2) ["ctr"]=> float(0.5) ["position"]=>
+    public function add_record($site_id, $date, $country, $device, $query, $page, $clicks, 
+                               $impressions, $ctr, $position){
+        $sql = "INSERT INTO `data` (`site_id`,`date`, `country`, `device`, `query`, `page`, 
+`clicks`, `impressions`, `ctr`, `position`) 
+        VALUES ($site_id,'$date', '$country', '$device', '$query', '$page', 
+        $clicks, $impressions, $ctr, $position)";
         if (!mysqli_query($this->connection, $sql))
             die('Error');
-    }
-    public function downloaded($key_id){
-        $sql = "UPDATE actions SET downloaded = 1 WHERE `id` = $key_id";
-        if (!mysqli_query($this->connection, $sql))
-            die('Error' . mysqli_error($this->connection));
-    }
-    public function audio_stats(){
-        $result = "";
-        $sql = "SELECT COUNT(*) as number, audio
-                FROM listening l 
-                INNER JOIN actions a
-                ON a.id = l.actions_id
-                WHERE `key` <> '" . self::CHECK_KEY . "'
-                GROUP BY audio";
-        $query = mysqli_query($this->connection, $sql);
-        while($row = $query->fetch_assoc())
-            $result .= '[' . '"audio '.$row['audio'].'"'.', '.$row['number'] . '],';
-        return $result;
-    }
-    public function keys_and_downloads_per_day(){
-        $result = "";
-        $sql = "SELECT COUNT(*) as number, COALESCE(SUM(downloaded),0) as downloaded, DATE_FORMAT(created, '%e-%m-%Y') as created_date 
-                FROM actions 
-                WHERE `key` <> '" . self::CHECK_KEY . "'
-                GROUP BY DATE_FORMAT(created, '%e-%m-%Y')
-                ORDER BY created";
-        $query = mysqli_query($this->connection, $sql);
-        while($row = $query->fetch_assoc())
-            $result .= '[' . '"'.$row['created_date'].'"'.', '.$row['number'].', '.$row['downloaded']. '],';
-        return $result;
     }
 }
--- load_data.php
+++ load_data.php
@@ -1,6 +1,7 @@
 <?php
 include_once __DIR__ . '/vendor/autoload.php';
 include_once "templates/base.php";
+include_once "db.php";
 
 echo pageHeader('Saving Data..');
 
@@ -17,25 +18,44 @@ $client->setAuthConfig($oauth_credentials);
 $client->setRedirectUri($redirect_uri);
 $client->addScope("https://www.googleapis.com/auth/webmasters");
 $client->setAccessToken($_SESSION['access_token']);
+$client_id = $_SESSION['client_id'];
+
 $service = new Google_Service_Webmasters($client);
 
 $websites = [];
 foreach ($service->sites->listSites()->getSiteEntry() as $siteEntry)
     $websites [] = $siteEntry['siteUrl'];
 
-foreach ($websites as $website){
-    $searchRequest = new Google_Service_Webmasters_SearchAnalyticsQueryRequest();
+$db = new Db();
 
-    $startDate = date('Y-m-d');
-    $endDate = date('Y-m-d', strtotime("-1 month"));
+foreach ($websites as $website){
+    $site_id = $db->is_client_website_exist($client_id, $website);
+
+    if($site_id == false)
+        $site_id = $db->add_website($client_id, $website);
+
+    $startDate = $db->get_last_record_date($site_id);
+
+    if($startDate == null)
+        $startDate = date('Y-m-d', strtotime("-3 month"));
+
+    $endDate = date('Y-m-d', strtotime("-1 day"));
+
+    $searchRequest = new Google_Service_Webmasters_SearchAnalyticsQueryRequest();
 
     $searchRequest->setStartDate($startDate);
     $searchRequest->setEndDate($endDate);
 
     try {
+        $searchRequest->setRowLimit(5);
         $searchRequest->setDimensions(["date", "country", "device", "query", "page"]);
         $data = $service->searchanalytics->query($website, $searchRequest);
         var_dump($data);
+        foreach ($data->getRows() as $row){
+            $db->add_record($site_id, $row->keys[0], $row->keys[1], $row->keys[2], $row->keys[3],
+                $row->clicks, $row->impressions, $row->ctr, $row->position);
+        }
+
     }
     catch (Exception $e){
         echo $e->getMessage();
